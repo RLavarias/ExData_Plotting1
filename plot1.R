a9a16e591c4d73df758fca357a29b84dd3c1d963
RLavarias <rommel.lavarias@gmail.com>
2015-12-13 17:30:54 -0500

    First commit

---
+++ plot1.R
@@ -0,0 +1,8 @@
+projectdata <- "household_power_consumption.txt"
+data <- read.table(projectdata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+datasubset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+globalActivePower <- as.numeric(datasubset$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,9 @@
+projectdata <- "household_power_consumption.txt"
+data <- read.table(projectdata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+datasubset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(datasubset$Date, datasubset$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(datasubset$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
